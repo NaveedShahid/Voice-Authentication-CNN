fdc81ef2337e7ad8e95a7fff5135d992ce32ebd8
drago314 <avrickaltmann@yahoo.com>
2023-07-29 21:48:16 -1000

    Allow Audio File Upload

--- project/__init__.py
+++ project/__init__.py
@@ -14,7 +14,7 @@ def create_app():
     db.init_app(app)
     with app.app_context():
         db.create_all()
-        
+
     login_manager = LoginManager()
     login_manager.login_view = 'auth.login'
     login_manager.init_app(app)
--- project/auth.py
+++ project/auth.py
@@ -1,10 +1,14 @@
-from flask import Blueprint, render_template, redirect, url_for, request, flash
+from flask import Blueprint, render_template, redirect, url_for, request, flash, Flask
 from werkzeug.security import generate_password_hash, check_password_hash
+from werkzeug.utils import secure_filename
 from .models import User
 from . import db
 from flask_login import login_user, login_required, logout_user
+import os
 
 auth = Blueprint('auth', __name__)
+UPLOAD_FOLDER = 'project/uploads'
+ALLOWED_EXTENSIONS = {'wav', 'm4a', 'mp3'}
 
 @auth.route('/login')
 def login():
@@ -33,7 +37,11 @@ def login_post():
 def signup():
     return render_template('signup.html')
 
-@auth.route('/signup', methods=['POST'])
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@auth.route('/signup',  methods=['POST'])
 def signup_post():
     # code to validate and add user to database goes here
     email = request.form.get('email')
@@ -46,6 +54,20 @@ def signup_post():
         flash('Email address already exists')
         return redirect(url_for('auth.signup'))
 
+    # check if the post request has the file part
+    if 'file' not in request.files:
+        flash('No file part')
+        return redirect(request.url)
+    file = request.files['file']
+    # If the user does not select a file, the browser submits an
+    # empty file without a filename.
+    if file.filename == '':
+        flash('No selected file')
+        return redirect(request.url)
+    if file and allowed_file(file.filename):
+        filename = secure_filename(file.filename)
+        file.save(os.path.join(UPLOAD_FOLDER, filename))
+    
     # create a new user with the form data. Hash the password so the plaintext version isn't saved.
     new_user = User(email=email, name=name, password=generate_password_hash(password, method='sha256'))
 
