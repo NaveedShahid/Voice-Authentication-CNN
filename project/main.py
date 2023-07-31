98a8ac4f6d4ef6287ae6eeae33faa56c7c4489ff
drago314 <avrickaltmann@yahoo.com>
2023-07-30 14:02:38 -1000

    Add About Page

--- project/main.py
+++ project/main.py
@@ -8,6 +8,10 @@ main = Blueprint('main', __name__)
 def index():
     return render_template('index.html')
 
+@main.route('/about')
+def about():
+    return render_template('about.html')
+
 @main.route('/profile')
 @login_required
 def profile():
